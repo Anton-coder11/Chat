c7e7505670cd06d7c7f290a0127a3470196146e3
Anton Oparin <gameslemon79@gmail.com>
2025-05-04 23:18:55 +0200

    ,

--- src/main/java/org/example/chat/ChatServer.java
+++ src/main/java/org/example/chat/ChatServer.java
@@ -1,10 +1,30 @@
 package org.example.chat;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.*;
 import java.net.*;
 
-public class ChatServer {
+public class ChatServer extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(ChatServer.class.getResource("ServerChat.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("AUUUUUUGram-Server");
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) throws IOException {
+        launch();
+        connecToServer();
+    }
+    public static void  connecToServer() throws IOException {
+
         ServerSocket serverSocket = new ServerSocket(12345);
         System.out.println("Сервер запущено, очікування клієнта...");
         Socket clientSocket = serverSocket.accept();
@@ -15,16 +35,20 @@ public class ChatServer {
 
         BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 
-        String message;
-        while ((message = in.readLine()) != null) {
-//            ChatController.sendServerMessage();
+
+        while (true){
+            String message = in.readLine();
+            if (message == null) {
+                break;
+            }
             System.out.println("Клієнт: " + message);
-            System.out.print("Ви: ");
+
+            System.out.print("Введіть повідомлення для клієнта: ");
             String response = userInput.readLine();
             out.println(response);
         }
-
         clientSocket.close();
         serverSocket.close();
     }
+
 }
